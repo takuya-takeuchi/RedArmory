b571ca2de5cda0c1a3e53c7e08b9845ae8fe9c96
takuya-takeuchi <spitzbergen@gmail.com>
2016-10-29 01:26:13 +0900

    Refactoring

--- source/RedArmory/Controls/MonthlyTriggerControl.xaml.cs
+++ source/RedArmory/Controls/MonthlyTriggerControl.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows.Controls;
-using MaterialDesignThemes.Wpf;
-using Ouranos.RedArmory.ViewModels;
 
 namespace Ouranos.RedArmory.Controls
 {
@@ -16,84 +14,6 @@ namespace Ouranos.RedArmory.Controls
         {
             InitializeComponent();
         }
-
-        #endregion
-
-        #region プロパティ
-
-        public MonthlyTriggerViewModel ViewModel
-        {
-            get
-            {
-                return this.DataContext as MonthlyTriggerViewModel;
-            }
-        }
-
-        #endregion
-
-        #region メソッド
-
-        #region イベントハンドラ
-
-        public void CalendarDialogOpenedEventHandler(object sender, DialogOpenedEventArgs eventArgs)
-        {
-            var trigger = this.ViewModel;
-            if (trigger == null)
-            {
-                return;
-            }
-
-            this._Calendar.SelectedDate = trigger.Date;
-        }
-
-        public void CalendarDialogClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
-        {
-            var trigger = this.ViewModel;
-            if (trigger == null)
-            {
-                return;
-            }
-
-            if (!Equals(eventArgs.Parameter, "1"))
-            {
-                return;
-            }
-
-            if (!this._Calendar.SelectedDate.HasValue)
-            {
-                eventArgs.Cancel();
-                return;
-            }
-
-            trigger.Date = this._Calendar.SelectedDate.Value;
-        }
-
-        public void ClockDialogOpenedEventHandler(object sender, DialogOpenedEventArgs eventArgs)
-        {
-            var trigger = this.ViewModel;
-            if (trigger == null)
-            {
-                return;
-            }
-
-            this._Clock.Time = trigger.Time;
-        }
-
-        public void ClockDialogClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
-        {
-            var trigger = this.ViewModel;
-            if (trigger == null)
-            {
-                return;
-            }
-
-            if (Equals(eventArgs.Parameter, "1"))
-            {
-                trigger.Time = this._Clock.Time;
-            }
-        }
-
-        #endregion
 
         #endregion
 

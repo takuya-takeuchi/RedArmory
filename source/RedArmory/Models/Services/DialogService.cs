41ac60f8ef3675aeb94262c2cf76773922730233
takuya-takeuchi <spitzbergen@gmail.com>
2016-11-05 23:25:57 +0900

    Create DialogService and IDialogService

---
+++ source/RedArmory/Models/Services/DialogService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Ouranos.RedArmory.Models.Services.Dialog;
+
+namespace Ouranos.RedArmory.Models.Services
+{
+
+    public sealed class DialogService : IDialogService
+    {
+
+        #region IDialogService メンバー
+
+        public async Task ShowMessage(MessageBoxButton button, string message, string title)
+        {
+            switch (button)
+            {
+                case MessageBoxButton.OK:
+                    await new OKDialogService().ShowMessage(message, title);
+                    break;
+                case MessageBoxButton.OKCancel:
+                    break;
+                case MessageBoxButton.YesNoCancel:
+                    break;
+                case MessageBoxButton.YesNo:
+                    await new YesNoDialogService().ShowMessage(message, title);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(button), button, null);
+            }
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ source/RedArmory/Models/Services/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ouranos.RedArmory.Models.Services
+{
+
+    public interface IDialogService
+    {
+
+        Task ShowMessage(MessageBoxButton button, string message, string title);
+
+    }
+
+}
